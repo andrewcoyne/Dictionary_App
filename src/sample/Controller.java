bc5ab470863a430baecab8909f8d567899d4c834
Drew <trix.krypt15@gmail.com>
2020-01-18 23:40:58 -0500

    worked on UI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.layout.AnchorPane;
+import org.controlsfx.control.textfield.TextFields;
+
 public class Controller {
+    private AnchorPane root;
+    private JFXTextField input;
+    public void initialize(){
+        String[] dictWords = {"hello", "hi"};
+        TextFields.bindAutoCompletion(input, dictWords);
+    }
 }
